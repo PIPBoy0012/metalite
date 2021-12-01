a412d2e60c9f19916cad64bf1858651deaaa800a
Iasemin-Florin <queue.triple@gmail.com>
2021-12-01 14:16:29 +0100

    worked on using the camera, having issues with it not being able to get launched on IOS and android for some reason

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,11 +2,16 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'posts.dart';
+import 'package:camera/camera.dart';
+import 'main.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  //final cameras = availableCameras();
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -14,6 +19,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   get title => widget.title;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:metalite/home_page.dart';
 
 import 'login_page.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+import 'dart:io';
+import 'posts.dart';
 
-void main() => runApp(MyApp());
+/*void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -19,5 +24,36 @@ class MyApp extends StatelessWidget {
         title: 'MetaLite',
       ),
     );
+  }
+}*/
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Getting list of cameras
+  final cameras = await availableCameras();
+
+  //Getting the first camera available
+  final firstCamera = cameras.first;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // ignore: recursive_getters
+  get firstCamera => firstCamera;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark(),
+        /*home: TakePictureScreen(
+        camera: firstCamera,
+      ),*/
+        home: HomePage(
+          title: 'metalite',
+        ));
   }
 }
